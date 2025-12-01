0d6f9f728e1a96548a28a20e689bad098ef6281f
mkrivic83 <krivic.milan@gmail.com>
2025-12-01 20:00:48 +0100

    projekt u repozitoriju

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Web Trgovina</title>
+    <link rel="stylesheet" href="css/stil.css">
+</head>
+<body>
+
+<nav>
+    <a href="index.php">Početna</a>
+    <a href="kategorije.php">Kategorije</a>
+    <a href="proizvodi.php">Proizvodi</a>
+</nav>
+<hr>
+<div id="content">
+    Ovdje će ići sadržaj
+
+    <h2>Proizvodi</h2>
+
+<h3>Web Trgovina</h3>
+</div>
+<div id="footer">
+<hr>
+<p>&copy; WebTrgovina i kompanija</p>
+</div>
+</body>
+</html>
